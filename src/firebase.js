e2121f8c4972419f16e78f16e65d8860ee1e8b45
hoon610 <123856266+hoon610@users.noreply.github.com>
2023-09-30 16:09:23 -0400

    Create header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAm86xnostTa8ZskL4NT-2stZD33Au_9-U",
+    authDomain: "facebook-clone-e6df9.firebaseapp.com",
+    projectId: "facebook-clone-e6df9",
+    storageBucket: "facebook-clone-e6df9.appspot.com",
+    messagingSenderId: "459541541813",
+    appId: "1:459541541813:web:07c184bf5faa53de8b9ffc",
+    measurementId: "G-H9BRTSTB6G"
+  };
