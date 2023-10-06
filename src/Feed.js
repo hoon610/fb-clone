1f29ecca560e4b3ef7d73caf0d7699abaea04a8e
hoon610 <123856266+hoon610@users.noreply.github.com>
2023-10-06 17:42:29 -0400

    Add commenting and liking functionality

--- src/Feed.js
+++ src/Feed.js
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import './Feed.css'
-import StoryReel from './StoryReel'
-import MessageSender from './MessageSender'
-import Post from './Post'
-import db from "./firebase"
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import StoryReel from "./StoryReel";
+import MessageSender from "./MessageSender";
+import Post from "./Post";
+import db from "./firebase";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection('posts').orderBy("timestamp", "desc").onSnapshot(snapshot => (
-      setPosts(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
-    ));
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })))
+      );
   }, []);
 
   return (
-    <div className='feed'>
-        <StoryReel />
-        <MessageSender />
-        {posts.map(post => (
-          <Post
-            key={post.id}
-            message={post.message}
-            timestamp={post.timestamp}
-            username={post.username}
-            image={post.image}
-            profilePic={post.profilePic}
-
-          />
-        ))}
+    <div className="feed">
+      <StoryReel />
+      <MessageSender />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          postId={post.id}
+          message={post.message}
+          timestamp={post.timestamp}
+          username={post.username}
+          image={post.image}
+          profilePic={post.profilePic}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import "./Widgets.css"
+import React from "react";
+import "./Widgets.css";
 
 function Widgets() {
   return (
-    <div className='widgets'>
+    <div className="widgets">
       <div className="text">
         <h2>Welcome to my Facebook Clone project!</h2>
         <p>
-          This project was created using React and Node.js. The backend authentication and database are managed by Firebase. Feel free to add your own posts!
+          This project was created using React and Node.js. The backend
+          authentication and database are managed by Firebase. Feel free to add
+          your own posts!
         </p>
-        <p>Currently, I have the functionality for adding posts to the timeline as authenticated (or guest) users. I am looking into adding additional functionality such as liking and commenting on posts. Additionally a way for users to add friends! Most of the UI buttons you see were a way for me to practice my React skills and hopefully their functionality will be fleshed out soon as well!</p>
         <p>
-          Check me out on <a href='https://github.com/hoon610' target='_blank' rel='noopener noreferrer'>
+          Currently, I have the functionality for adding posts to the timeline
+          and commenting as authenticated (or guest) users. Authenticated user
+          can also like posts as well. I am looking into adding additional
+          functionality such as a way for users to add friends! Most of the UI
+          buttons you see were a way for me to practice my React skills and
+          hopefully their functionality will be fleshed out soon as well!
+        </p>
+        <p>
+          Check me out on{" "}
+          <a
+            href="https://github.com/hoon610"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             GitHub
           </a>
         </p>
         <p>Thank you for visiting!</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
